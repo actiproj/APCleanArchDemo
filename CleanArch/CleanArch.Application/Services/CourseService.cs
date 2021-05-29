ce9cb274ad1364af42e2a416d4e30a9e8e5a3e0a
Actiproj <info@actiproj.com>
2021-05-29 11:30:27 +0200

    Course Service Cretae functionality, Updated CourseViewModel

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,23 @@ namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description, 
+                courseViewModel.ImageUrl
+                );
+
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()
@@ -24,5 +39,6 @@ namespace CleanArch.Application.Services
                 Courses = _courseRepository.GetCourses()
             };
         }
+
     }
 }
